a0fe06a64a5b33734815fb18e4f41d3de573cd9d
vrakosky <vrakosky@MacBook-Pro-de-vincent.local>
2018-01-29 21:52:24 +0100

    Update DisqueTest.java : Test de la methode compareTo (OK)

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -11,9 +11,20 @@ public class DisqueTest {
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
+                
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+                assertEquals(-1, petit.compareTo(grand));
+
+
+                assertEquals(0, petit.compareTo(petit));
+                assertEquals(0, moyen.compareTo(moyen));
+                assertEquals(0, grand.compareTo(grand));
+
+            
+                assertEquals(1, moyen.compareTo(petit));
+                assertEquals(1, grand.compareTo(moyen));	    
+                assertEquals(1, grand.compareTo(petit));             
 	}
 
 }
